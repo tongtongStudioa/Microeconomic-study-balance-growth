6656df0133671f89d673dfc36aec05b56c561b65
tongtongStudioa <>
2025-01-14 19:55:26 +0100

    make simulation work for one account

--- account_modilzation_balance.py
+++ account_modilzation_balance.py
@@ -24,13 +24,19 @@ class BalancingSimulation:
         #todo : tests multiple scenarios to compare investment ratio and saving ratio etc
         return
 class BankAccount:
-    def __init__(self,initial_balance = 500,initial_saving = 250, initial_investment = 100,monthly_income = 2500):
+    """ Create life bank account for particular personn with all parameters you want."""
+    
+    def __init__(self,initial_balance = 1500,initial_saving = 2250, initial_investment = 1100,monthly_income = 2500):
         self.initial_balance = initial_balance
         self.initial_saving = initial_saving
         self.initial_investment = initial_investment
         self.monthly_income = monthly_income
         self.initial_capital = initial_balance + initial_investment + initial_saving
-        self.init_param = False
+        self.monthly_expenses_ratio = 0.85
+        self.monthly_expenses = self.monthly_expenses_ratio * self.monthly_income   # Dépenses fixes mensuelles ($)
+        self.saving_ratio = 0.2
+        self.investment_ratio = 0.3
+        self.init_user_param = False
         
     def init_parameters(self):
         monthly_expenses_ratio = float(input("Taux de dépenses mensuel (en %) :"))/100 # Propension marginale à consommer
@@ -41,15 +47,16 @@ class BankAccount:
         self.monthly_expenses = monthly_expenses
         self.saving_ratio = saving_ratio
         self.investment_ratio = investment_ratio
-        # Signaler que les paramètres essentiels sont mis à jour
-        self.init_param = True
+        # Signaler que les paramètres essentiels sont mis à jour par l'utilisateur
+        self.init_user_param = True
         
     def __str__(self):
-        print(f"\nMonthly income : {self.monthly_income} $")
-        print(f"Monthly expenses : {self.monthly_expenses} $")
-        print(f">>> Capital initial : {self.capital:.1f} $ <<<\n\n")
-                
-    def simulation(self,months_n,investment_return_rate = 0.05,uncertainty = 0.2,saving_loss_prob = 0.01,investment_loss_prob = 0.15):
+        infos = f"\nMonthly income : {self.monthly_income} $\n"
+        infos += f"Monthly expenses : {self.monthly_expenses} $\n"
+        infos += f">>> Capital initial : {self.capital:.1f} $ <<<\n\n"
+        return infos
+    
+    def simulation(self,months_n,investment_return_rate = 0.05,saving_return_rate = 0.025, expense_uncertainty = 0.2, income_uncertainty = 0.05,saving_loss_prob = 0.01,investment_loss_prob = 0.15):
         """Simulation for a number of months"""
         
         self.balances = [self.initial_balance]  # Liste pour stocker le solde à chaque mois
@@ -75,16 +82,19 @@ class BankAccount:
             current_balance = self.balances[-1]
             
             # Ajouter revenu et déduire dépenses
-            rate = np.random.uniform(-uncertainty,uncertainty) # Ajoute l'incertitude aux revenus et dépenses
+            rate = np.random.uniform(-income_uncertainty, income_uncertainty) # Ajoute l'incertitude aux revenus et dépenses
             income = self.monthly_income * (1 + rate)
-            rate = np.random.uniform(-uncertainty,uncertainty)
+            rate = np.random.uniform(-expense_uncertainty, expense_uncertainty)
             expense = self.monthly_expenses * (1 + rate)
             
             # Ajouter les gains réinvestis
             if (i >= 12 and self.investments[i-12] > 0):
                 print(f"{self.investments[i-12]:.1f} $ was invested one year ago at {investment_return_rate * 100}%")
                 income += self.investments[i-12] * investment_return_rate
-            
+            if (i >= 12 and self.savings[i-12] > 0):
+                print("f{self.savings[i-12]:.1} $ save at {saving_return_rate * 100}%")
+                income += self.savings[i-12] * saving_return_rate
+                
             new_balance = current_balance + income - expense
             print(f"Balance on {months[i%12]}: {new_balance:.1f} $")
             
@@ -102,14 +112,13 @@ class BankAccount:
             # Epargner et investir si possible
             monthly_saving = 0
             if (new_balance > 0):
-                # Interet de 3% par ans
-                monthly_saving = new_balance * self.saving_ratio * choice([0,1], 1, p=[0.01,0.99])[0] #1 % de chance de perte 
+                monthly_saving = new_balance * self.saving_ratio * choice([0,1], 1, p=[saving_loss_prob ,1 - saving_loss_prob])[0] #1 % de chance de perte 
                 #print(f"Epargne mensuel : {monthly_saving:.1f} $")
                 new_balance -= monthly_saving
                 
             monthly_investment = 0
             if (new_balance > 0 and new_balance * self.investment_ratio > 100):
-                monthly_investment = new_balance * self.investment_ratio * choice([0,1], 1, p=[0.15,0.85])[0] #15 % de chance de perte 
+                monthly_investment = new_balance * self.investment_ratio * choice([0,1], 1, p=[investment_loss_prob ,1 - investment_loss_prob])[0] #15 % de chance de perte 
                 new_balance -= monthly_investment
                 #print(f"Investissement mensuel : {monthly_investment:.1f} $")
                 #print(f"Balance after investments/savings on {months[i%12]}: {new_balance:.1f} $")
---
+++ main.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 14 18:42:09 2025
+
+@author: La famille tong
+"""
+
+from account_modilzation_balance import BankAccount 
+
+account1 = BankAccount()
+account1.init_parameters()
+account1.simulation(12*10)
